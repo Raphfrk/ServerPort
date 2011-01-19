23e25a08ea24b556c0b0dafcaec4d4a415b0036f
Raphfrk <raphfrk@gmail.com>
2011-01-19 20:39:08 +0000

    added patcher source directories

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "188";
+	final static String version = "189";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
