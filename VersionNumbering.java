75751c66b545862d70d12ddc623c7de2f81b5859
Raphfrk <raphfrk@gmail.com>
2011-01-24 18:56:21 +0000

    fixed holding bug and redirect bug

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "197";
+	final static String version = "198";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
