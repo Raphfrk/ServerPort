631a1123f525f2d3fd6a14179395787c59321983
Raphfrk <raphfrk@gmail.com>
2011-01-26 23:05:53 +0000

    might have fixed stuff :) - damage spam

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "202";
+	final static String version = "203";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
