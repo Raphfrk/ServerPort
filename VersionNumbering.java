ff11e928cbe545ba858d109dbdfa72013fa01c2e
Raphfrk <raphfrk@gmail.com>
2011-01-27 22:43:51 +0000

    updated to latest server

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "205";
+	final static String version = "206";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
