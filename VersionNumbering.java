dd27cb8424f131193297a0f13bdd0526ecf82279
Raphfrk <raphfrk@gmail.com>
2011-01-27 20:08:24 +0000

    added password protection for proxies

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "204";
+	final static String version = "205";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
