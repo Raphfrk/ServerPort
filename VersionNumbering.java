48a70bbcc1d93bda6aed7dd7f74faf9ad1f2a066
Raphfrk <raphfrk@gmail.com>
2011-02-02 18:18:13 +0000

    Added localhost fix for 5 second login timeout

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "218";
+	final static String version = "219";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
