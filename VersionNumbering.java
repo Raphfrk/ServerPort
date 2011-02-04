01e5531973221f0453239a6cdc72bb0745404f58
Raphfrk <raphfrk@gmail.com>
2011-02-04 19:18:53 +0000

    Converted CraftProxy to a plugin

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "225";
+	final static String version = "226";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
