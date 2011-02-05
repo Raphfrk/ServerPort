49d9d67b715b0d4f5c99a7ae540d030c196851de
Raphfrk <raphfrk@gmail.com>
2011-02-05 21:43:37 +0000

    automatically connects to the last server connected to

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "231";
+	final static String version = "232";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
