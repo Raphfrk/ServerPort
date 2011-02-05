95a9e2082654d42a4e7afcbab2e617eb7fae3b8f
Raphfrk <raphfrk@gmail.com>
2011-02-05 19:45:08 +0000

    Removed debug commands from CraftProxy

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "230";
+	final static String version = "231";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
