15e7fcc0297e38616e5bb8a64f1d1996c7631b52
Raphfrk <raphfrk@gmail.com>
2011-02-05 21:47:20 +0000

    added more command line comments to the proxy

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "232";
+	final static String version = "233";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
