48f03fd8fe0f7f4b464ebddadecfd3e454fae4fe
Raphfrk <raphfrk@gmail.com>
2011-02-11 20:01:49 +0000

    fire activation fix - hopefully

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "243";
+	final static String version = "244";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
