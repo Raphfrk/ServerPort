896ed6d6ee883b8596a6468f028b6bfa17cbd8e2
Raphfrk <raphfrk@gmail.com>
2011-02-13 04:44:55 +0000

    gate use permissions fix

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "246";
+	final static String version = "247";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
