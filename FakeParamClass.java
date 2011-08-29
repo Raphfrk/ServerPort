887a61629a583c2d3d2063625d2bdabeaea6b19a
raphfrk <raphfrk@gmail.com>
2011-08-29 18:20:16 +0100

    file deletes

--- FakeParamClass.java
+++
@@ -1,8 +0,0 @@
-
-public class FakeParamClass {
-	
-	FakeParamClass() {
-		
-	}
-
-}
--- VersionNumbering.java
+++ VersionNumbering.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "359";
+	final static String version = "382";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
